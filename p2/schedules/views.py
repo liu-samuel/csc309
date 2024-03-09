93450f59a68e425980100b0ddace311e0c9486a6
elsiezhu <elsie.zhu2003@gmail.com>
2024-03-09 17:59:39 -0500

    add email functionality

--- p2/schedules/views.py
+++ p2/schedules/views.py
@@ -8,10 +8,11 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Event
 from .serializers import EventSerializer
 from django.core import serializers
+from django.core import mail
 
 import json
 
-class EventsListAPIView(generics.CreateAPIView):
+class EventsListAPIView(generics.ListCreateAPIView):
     # permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, **kwargs):
@@ -29,7 +30,7 @@ class EventsListAPIView(generics.CreateAPIView):
             events = Event.objects.filter(owner__pk=owner, invitee__pk=invitee, is_finalized=bool(is_finalized))
         except Exception:
             return Response({'error': 'Cannot get availabilities for event'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         try:
             events_response = serializers.serialize('json', events)
             return Response(json.loads(events_response), status=status.HTTP_200_OK)
@@ -66,11 +67,23 @@ class EventsListAPIView(generics.CreateAPIView):
 
         serializer = EventSerializer(data=event_data)
         if serializer.is_valid():
-            serializer.save()
+            event = serializer.save()
             response = Response(serializer.data, status=status.HTTP_201_CREATED)
 
-            # TODO: send email request
+            send_request_email(owner=owner_user, invitee=invitee_user, event=event)
             return response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+def send_request_email(owner, invitee, event):
+    owner_email = owner.email
+    invitee_email = invitee.email
+    subject = f"{owner.email} has invited you to input availability for '{event.name}'"
+    description = f"add link here"
+    with mail.get_connection() as connection:
+        mail.EmailMessage(
+            subject,
+            description,
+            owner_email,
+            [invitee_email],
+            connection=connection,
+        ).send()
