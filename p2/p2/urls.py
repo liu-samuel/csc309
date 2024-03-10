928081ce69f3455e5d7b6968338b173ba883f4b8
Luke Cheseldine <cheeseldine@gmail.com>
2024-03-10 19:27:58 -0400

    swagger bang

--- p2/p2/urls.py
+++ p2/p2/urls.py
@@ -14,19 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import permissions
+from django.urls import path, include, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Your API",
+      title="API Documentation",
       default_version='v1',
-      description="Your API description",
-      terms_of_service="https://www.example.com/policies/terms/",
-      contact=openapi.Contact(email="contact@example.com"),
-      license=openapi.License(name="BSD License"),
+      description="API documentation for the project",
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
@@ -36,6 +33,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  
     path('schedules/', include('schedules.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
