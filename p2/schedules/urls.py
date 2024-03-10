61535454c83510ab447846327984c61328b38eca
Luke Cheseldine <cheeseldine@gmail.com>
2024-03-10 19:17:07 -0400

    remove backtick

--- p2/schedules/urls.py
+++ p2/schedules/urls.py
@@ -6,6 +6,6 @@ app_name = 'schedules'
 urlpatterns = [
     path('events/', EventsListAPIView.as_view(), name='events'),
     path('events/<int:event_id>/availability/', EventAvailabilityAPIView.as_view(), name="event_availability"),
-    path('events/<int:event_id>/', EventAPIView.as_view(),  name="events_id"),`
+    path('events/<int:event_id>/', EventAPIView.as_view(),  name="events_id"),
     path('events/<int:event_id>/suggestion/', SuggestionAPIView.as_view(), name="suggestion")
 ]
