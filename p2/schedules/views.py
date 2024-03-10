d81fa1d3bc87c31b136e701b4132202d30eda249
elsiezhu <elsie.zhu2003@gmail.com>
2024-03-10 18:00:04 -0400

    done

--- p2/schedules/views.py
+++ p2/schedules/views.py
@@ -1,3 +1,4 @@
+from .utils import send_email
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics, permissions
@@ -8,18 +9,39 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Event
 from .serializers import EventSerializer
 from django.core import serializers
-from django.core import mail
 
 import json
 
 class EventsListAPIView(generics.ListCreateAPIView):
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, **kwargs):
         owner = request.query_params.get("owner")
         invitee = request.query_params.get("invitee")
         is_finalized = request.query_params.get("is_finalized")
         
+        # validations
+
+        # check for missing params
+        missing_params = []
+        if not owner:
+            missing_params.append('owner')
+        if not invitee:
+            missing_params.append('invitee')
+        if not is_finalized:
+            missing_params.append('is_finalized')
+        if len(missing_params) > 0:
+            return Response({'error': f"Missing required parameters {missing_params.join(', ')}"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # check that the current user is either the owner or the invitee
+        current_user = request.user
+        if (current_user.pk != owner) and (current_user.pk != invitee):
+            return Response({'error': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        
+        # check that owner and invitee are different
+        if owner == invitee:
+            return Response({'error': 'owner and invitee must be different'}, status=status.HTTP_400_BAD_REQUEST)
+
         if is_finalized:
             if is_finalized.lower() == "false":
                 is_finalized = False
@@ -39,25 +61,43 @@ class EventsListAPIView(generics.ListCreateAPIView):
 
 
     def post(self, request, **kwargs):
-        owner = request.data.get("owner")
+        owner = request.user
         invitee = request.data.get("invitee")
         deadline = request.data.get("deadline")
         name = request.data.get("name")
         is_finalized = False
         selected_time = None
         
-        # validations
+        # VALIDATIONS
+
+        # check that all required fields exist
+        missing_params = []
+        if not invitee:
+            missing_params.append('invitee')
+        if not deadline:
+            missing_params.append('deadline')
+        if len(missing_params) > 0:
+            return Response({'error': f"Missing required parameters {missing_params.join(', ')}"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # check that the two users are different
+        if owner.pk == invitee:
+            return Response({'error': 'Can\'t invite yourself'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # check that invitee user ID exists
         try:
-            owner_user = get_user_model().objects.get(pk=owner)
             invitee_user = get_user_model().objects.get(pk=invitee)
 
             if not name: # necessary due to serializer
-                name = f'{owner_user.first_name} / {invitee_user.first_name}'
+                name = f'{owner.first_name} / {invitee_user.first_name}'
         except get_user_model().DoesNotExist:
             return Response({'error': 'User(s) not found'}, status=status.HTTP_404_NOT_FOUND)
         
+        # check that the owner and invitee are contacts
+        if invitee_user not in owner.friends.all():
+            return Response({'error': 'Must be contacts to create an event invitation'}, status=status.HTTP_403_FORBIDDEN)
+        
         event_data = {
-            "owner": owner,
+            "owner": owner.pk,
             "invitee": invitee,
             "deadline": deadline,
             "name": name,
@@ -69,8 +109,11 @@ class EventsListAPIView(generics.ListCreateAPIView):
         if serializer.is_valid():
             event = serializer.save()
             response = Response(serializer.data, status=status.HTTP_201_CREATED)
-
-            send_request_email(owner=owner_user, invitee=invitee_user, event=event)
+            try:
+                send_request_email(owner=owner, invitee=invitee_user, event=event)
+            except:
+                return Response({'error': 'Could not send email'}, status=status.HTTP_400_BAD_REQUEST)
+            
             return response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -78,12 +121,6 @@ def send_request_email(owner, invitee, event):
     owner_email = owner.email
     invitee_email = invitee.email
     subject = f"{owner.email} has invited you to input availability for '{event.name}'"
-    description = f"add link here"
-    with mail.get_connection() as connection:
-        mail.EmailMessage(
-            subject,
-            description,
-            owner_email,
-            [invitee_email],
-            connection=connection,
-        ).send()
+    body = f"add link here"
+
+    send_email(owner_email, [invitee_email], subject, body)
