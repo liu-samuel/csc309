f44a153b4c7f9805b44c6fd7d3a92d3358c5bd2b
Luke Cheseldine <cheeseldine@gmail.com>
2024-04-04 12:38:22 -0700

    done

--- frontend/src/components/HomeCalendar/HomeCalendar.jsx
+++ frontend/src/components/HomeCalendar/HomeCalendar.jsx
@@ -1,21 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import './HomeCalendar.css'
+import { useNavigate } from 'react-router-dom'
 
-const HomeCalendar = () => {
+const HomeCalendar = ({ events }) => {
+    const navigate = useNavigate()
     const [currentDate, setCurrentDate] = useState(new Date())
     const [displayDays, setDisplayDays] = useState(4)
-
-    // Placeholder for fetching events from the backend
-    const fetchEvents = () => {
-        // TODO: Implement event fetching logic
-        return []
-    }
-
-    const fetchMeetingRequests = () => {}
-
-    const fetchProposedMeetings = () => {}
-
-    const events = fetchEvents()
 
     const goToNextDay = () => {
         setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() + 1)))
@@ -26,7 +16,7 @@ const HomeCalendar = () => {
     }
 
     const goToEventDetail = eventId => {
-        // TODO: Navigate to event's detail page
+        navigate(`/meeting_details/${eventId}`)
     }
 
     useEffect(() => {
@@ -64,14 +54,92 @@ const HomeCalendar = () => {
         return dates
     }
 
+    // Function to check if an event is on a given date and time
+    const isEventOnDateTime = (event, date) => {
+        const eventDate = new Date(event.selected_time)
+        return (
+            eventDate.toDateString() === date.toDateString() &&
+            eventDate.getHours() === date.getHours() &&
+            eventDate.getMinutes() === date.getMinutes()
+        )
+    }
+
+    const getRandomBackgroundColor = () => {
+        const colors = [
+            '#FFCCCB',
+            '#FFE4CC',
+            '#FFD1FF',
+            '#FFFFCC',
+            '#99D6EA',
+            '#EAD9C9',
+            '#99A3FF',
+            '#CCC2A3',
+            '#CCFFCC',
+            '#D1A3A3',
+            '#B3D699',
+            '#D1D18C',
+            '#FFDADA',
+            '#A3B3D6',
+            '#B3D1A3',
+        ]
+        return colors[Math.floor(Math.random() * colors.length)]
+    }
+
+    const renderCalendarCells = () => {
+        const rows = []
+        for (let hour = 0; hour < 24; hour++) {
+            for (let half = 0; half < 2; half++) {
+                // Add loop for half-hour intervals
+                const row = []
+                const timeString = `${hour.toString().padStart(2, '0')}:${half === 0 ? '00' : '30'}`
+                row.push(<td className='time'>{timeString}</td>) // Time column
+
+                for (let i = 0; i < displayDays; i++) {
+                    const date = new Date(currentDate)
+                    date.setDate(currentDate.getDate() + i)
+                    date.setHours(hour, half * 30, 0, 0) // Set minutes to 0 or 30
+
+                    const event =
+                        Array.isArray(events) &&
+                        events.find(e => isEventOnDateTime(e, date))
+                    if (event) {
+                        row.push(
+                            <td
+                                key={`${hour}-${half}-${i}`}
+                                className='with-meeting'
+                                style={{
+                                    backgroundColor: getRandomBackgroundColor(),
+                                }}
+                                onClick={() => goToEventDetail(event.id)} // Add click handler
+                            >
+                                <div className='meeting-tile'>
+                                    {event.name}
+                                    <div className='meeting-tile-subtext'>
+                                        with {event.otherUser}
+                                    </div>
+                                </div>
+                            </td>
+                        )
+                    } else {
+                        row.push(<td key={`${hour}-${half}-${i}`}></td>)
+                    }
+                }
+                rows.push(<tr key={`${hour}-${half}`}>{row}</tr>)
+            }
+        }
+        return rows
+    }
+
     return (
         <div className='calendar-container'>
             <table>
                 <thead>
                     <tr className='header'>{renderHeaderDates()}</tr>
                 </thead>
+
                 <tbody>
-                    <tr>
+                    {events && renderCalendarCells()}
+                    {/* <tr>
                         <td className='time'>00:00</td>
                         <td></td>
                         <td></td>
@@ -268,7 +336,7 @@ const HomeCalendar = () => {
                         <td></td>
                         <td></td>
                         <td></td>
-                    </tr>
+                    </tr> */}
                 </tbody>
             </table>
         </div>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,45 +1,264 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import NavBar from '../../components/NavBar/NavBar'
 import HomeCalendar from '../../components/HomeCalendar/HomeCalendar'
+import { useAuth } from '../../contexts/AuthContext'
+import { EVENT_URL, USER_URL } from '../../constants'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+    const { user } = useAuth()
+    const navigate = useNavigate()
+    const [events, setEvents] = useState([])
+    const [meetingRequests, setMeetingRequests] = useState([])
+    const [proposedMeetings, setProposedMeetings] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!user) return
+
+            try {
+                const fetchedEvents = await fetchEvents()
+                setEvents(fetchedEvents)
+
+                const fetchedMeetingRequests = await fetchMeetingRequests()
+                setMeetingRequests(fetchedMeetingRequests)
+
+                const fetchedProposedMeetings = await fetchProposedMeetings()
+                setProposedMeetings(fetchedProposedMeetings)
+            } catch (error) {
+                console.error('Error fetching data:', error)
+            }
+            console.log('events', events)
+            console.log('meetingRequests', meetingRequests)
+            console.log('propsedMeetings', proposedMeetings)
+        }
+
+        fetchData()
+    }, [user]) // Dependency array, re-run the effect when `user` changes
+
+    // Helper function to fetch user details
+    const fetchUserDetails = async userId => {
+        try {
+            const response = await fetch(`${USER_URL}${userId}/`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${user.token}`,
+                },
+            })
+            const userDetails = await response.json()
+            return userDetails.name
+        } catch (error) {
+            console.error('Error fetching user details:', error)
+            return null
+        }
+    }
+
+    const fetchEvents = async () => {
+        if (!user) return []
+
+        try {
+            const eventsWhereOwnerRes = await fetch(
+                `${EVENT_URL}?owner=${user.id}&is_finalized=true`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                }
+            )
+            const eventsWhereOwner = await eventsWhereOwnerRes.json()
+
+            const eventsWhereInviteeRes = await fetch(
+                `${EVENT_URL}?invitee=${user.id}&is_finalized=true`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                }
+            )
+            const eventsWhereInvitee = await eventsWhereInviteeRes.json()
+
+            // Combine the events and map over them to replace the user ID with the name
+            const events = [
+                ...eventsWhereOwner.events,
+                ...eventsWhereInvitee.events,
+            ].map(async event => {
+                // Check if the current user is the owner or invitee and fetch the other user's details
+                const otherUserId =
+                    user.id === event.owner ? event.invitee : event.owner
+                const otherUserName = await fetchUserDetails(otherUserId)
+                return {
+                    ...event,
+                    otherUser: otherUserName, // Add the other user's name to the event object
+                }
+            })
+
+            // Resolve all promises from the map (since fetchUserDetails is async)
+            const eventsWithNames = await Promise.all(events)
+            return eventsWithNames
+        } catch (error) {
+            console.error('Error fetching events:', error)
+            return []
+        }
+    }
+
+    const fetchMeetingRequests = async () => {
+        if (!user) return []
+
+        try {
+            const meetingRequestsRes = await fetch(
+                `${EVENT_URL}?invitee=${user.id}&is_finalized=false`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                }
+            )
+            const meetingRequestsJson = await meetingRequestsRes.json()
+            const meetingRequests = meetingRequestsJson.events.map(
+                async event => {
+                    // Assuming the owner is the other user in this context
+                    const otherUserName = await fetchUserDetails(event.owner)
+                    return {
+                        ...event,
+                        otherUser: otherUserName,
+                    }
+                }
+            )
+
+            // Resolve all promises from the map
+            const meetingRequestsWithNames = await Promise.all(meetingRequests)
+            return meetingRequestsWithNames
+        } catch (error) {
+            console.error('Error fetching meeting requests:', error)
+            return []
+        }
+    }
+
+    const fetchProposedMeetings = async () => {
+        if (!user) return []
+
+        try {
+            const proposedMeetingsRes = await fetch(
+                `${EVENT_URL}?owner=${user.id}&is_finalized=false`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                }
+            )
+            const proposedMeetingsJson = await proposedMeetingsRes.json()
+            const proposedMeetings = proposedMeetingsJson.events.map(
+                async event => {
+                    // Assuming the invitee is the other user in this context
+                    const otherUserName = await fetchUserDetails(event.invitee)
+                    return {
+                        ...event,
+                        otherUser: otherUserName,
+                    }
+                }
+            )
+
+            // Resolve all promises from the map
+            const proposedMeetingsWithNames =
+                await Promise.all(proposedMeetings)
+            return proposedMeetingsWithNames
+        } catch (error) {
+            console.error('Error fetching proposed meetings:', error)
+            return []
+        }
+    }
+
     return (
         <div className='full-page'>
             <NavBar />
             <main className='content'>
                 <div className='calendar'>
-                    <HomeCalendar />
+                    <HomeCalendar events={events} />
                 </div>
                 <div className='meetings'>
                     <div className='meeting-requests'>
                         <h3>Meeting Requests</h3>
-                        <div className='meeting-request'>
-                            <span className='title'>Sprint Retro</span>
-                            <span className='requester'>with Samuel Liu</span>
-                            <span className='due'>due Jan 14, 2:00PM</span>
-                            <button
-                                className='add-availability button-primary'
-                                onclick='viewMeetingDetails()'
-                            >
-                                Enter your availability
-                            </button>
-                        </div>
+                        {meetingRequests.length > 0 ? (
+                            meetingRequests.map(request => (
+                                <div
+                                    className='meeting-request'
+                                    key={request.id}
+                                >
+                                    <span className='title'>
+                                        {request.name}
+                                    </span>
+                                    <span className='requester'>
+                                        with {request.otherUser}
+                                    </span>
+                                    <span className='due'>
+                                        due{' '}
+                                        {new Date(
+                                            request.deadline
+                                        ).toLocaleString()}
+                                    </span>
+                                    <button
+                                        className='add-availability button-primary'
+                                        onClick={() =>
+                                            navigate(
+                                                `/meeting_details/${request.id}`
+                                            )
+                                        }
+                                    >
+                                        Enter your availability
+                                    </button>
+                                </div>
+                            ))
+                        ) : (
+                            <p>You have no pending meeting requests.</p>
+                        )}
                     </div>
-                    <div class='proposed-meetings'>
-                        <h3 class='white'>Proposed Meetings</h3>
-                        <div class='proposed-meeting'>
-                            <span class='title'>Run Club</span>
-                            <span class='requester'>
-                                with Elsie Zhu, Ron Varshavsky, and Samuel Liu
-                            </span>
-                            <button
-                                class='add-availability button-secondary'
-                                onclick='viewMeetingDetails()'
-                            >
-                                View
-                            </button>
-                        </div>
+                    <div className='proposed-meetings'>
+                        <h3 style={{ color: 'white' }}>Proposed Meetings</h3>
+                        {proposedMeetings.length > 0 ? (
+                            proposedMeetings.map(meeting => (
+                                <div
+                                    className='proposed-meeting'
+                                    key={meeting.id}
+                                >
+                                    <span className='title'>
+                                        {meeting.name}
+                                    </span>
+                                    <span className='requester'>
+                                        with {meeting.otherUser}
+                                    </span>
+                                    <span className='due'>
+                                        due{' '}
+                                        {new Date(
+                                            meeting.selected_time
+                                        ).toLocaleString()}
+                                    </span>
+                                    <button
+                                        className='add-availability button-secondary'
+                                        onClick={() =>
+                                            navigate(
+                                                `/meeting_details/${meeting.id}`
+                                            )
+                                        }
+                                    >
+                                        View
+                                    </button>
+                                </div>
+                            ))
+                        ) : (
+                            <p style={{ color: 'white' }}>
+                                You have no unfinished proposed meetings.
+                            </p>
+                        )}
                     </div>
                 </div>
             </main>
