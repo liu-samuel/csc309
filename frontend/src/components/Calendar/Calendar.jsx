f762b9fdbeecac5bcffbfd38f211d82e2efe6b1e
Ron Varshavsky <ronvarshavsky1@gmail.com>
2024-04-05 00:47:14 -0400

    Implemented saving and loading meeting details

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -1,70 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Calendar.css";
 import axios from "axios";
-import { EVENT_AVAILABILITY_URL, TOKEN_URL } from "../../constants";
+import { EVENT_AVAILABILITY_URL, EVENT_URL, TOKEN_URL } from "../../constants";
 
-class Calendar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      startDate: new Date(),
-      token: "",
-      logged_in_email: "user1@user1.com",
-      calendarItems: Array.from({ length: 48 }, (_, i) => {
-        return {
-          key: i / 2,
-          hour: Math.floor(i / 2),
-          time:
-            String(Math.floor(i / 2)).padStart(2, "0") +
-            (i % 2 == 0 ? ":00" : ":30"),
-          items: Array.from({ length: 4 }, (_, j) => {
-            return {
-              time_start:
-                String(
-                  new Date(this.calculateDayDate(new Date(), j))
-                    .toISOString()
-                    .slice(0, 10)
-                ) +
-                "T" +
-                String(Math.floor(i / 2)).padStart(2, "0") +
-                (i % 2 == 0 ? ":00" : ":30"),
-              availability: "",
-            };
-          }),
-        };
-      }),
-    };
-  }
-
-  async fetchToken() {
+const Calendar = (props) => {
+  const fetchToken = async () => {
     try {
       const response = await axios.post(`${TOKEN_URL}`, {
         username: "user1",
         password: "password",
       });
-      this.setState({ token: response.data.access });
+      setToken(response.data.access);
       return response.data.access;
     } catch (error) {
       console.error("Error fetching token: ", error);
     }
-  }
+  };
 
-  async setAvailability(email, token) {
+  const setAvailability = async (email, token) => {
     try {
-      for (var i = 0; i < this.state.calendarItems[0].items.length; i++) {
-        const end_date = new Date(
-          this.state.calendarItems[0].items[i].time_start
-        );
+      for (var i = 0; i < calendarItems[0].items.length; i++) {
+        const end_date = new Date(calendarItems[0].items[i].time_start);
         end_date.setDate(end_date.getDate() + 1);
         end_date.setUTCHours(0);
         const body = JSON.stringify({
           email: email,
-          start_time: this.state.calendarItems[0].items[i].time_start,
+          start_time: calendarItems[0].items[i].time_start,
           end_time: end_date.toISOString().slice(0, 16),
         });
 
         const response = await axios.delete(
-          `${EVENT_AVAILABILITY_URL(this.props.event_id)}`,
+          `${EVENT_AVAILABILITY_URL(props.event_id)}`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -80,7 +46,7 @@ class Calendar extends React.Component {
       }
 
       const body_request = [];
-      this.state.calendarItems.forEach((row) => {
+      calendarItems.forEach((row) => {
         row.items.forEach(async (cell) => {
           if (cell.availability !== "") {
             // add 30 mins
@@ -107,7 +73,7 @@ class Calendar extends React.Component {
       });
 
       await axios.post(
-        `${EVENT_AVAILABILITY_URL(this.props.event_id)}`,
+        `${EVENT_AVAILABILITY_URL(props.event_id)}`,
         availability_data,
         {
           headers: {
@@ -121,9 +87,9 @@ class Calendar extends React.Component {
     } catch (error) {
       console.error("Error adding availability: ", error);
     }
-  }
+  };
 
-  updateCalendarItem = (new_date) => {
+  const resetCalendarItem = (new_date) => {
     return Array.from({ length: 48 }, (_, i) => {
       return {
         key: i / 2,
@@ -135,7 +101,7 @@ class Calendar extends React.Component {
           return {
             time_start:
               String(
-                new Date(this.calculateDayDate(new_date, j))
+                new Date(calculateDayDate(new_date, j))
                   .toISOString()
                   .slice(0, 10)
               ) +
@@ -149,29 +115,29 @@ class Calendar extends React.Component {
     });
   };
 
-  nextWeek = () => {
-    const { startDate } = this.state;
+  const nextWeek = () => {
     const nextWeekStartDate = new Date(startDate);
     nextWeekStartDate.setDate(nextWeekStartDate.getDate() + 4);
-    const newCalendar = this.updateCalendarItem(nextWeekStartDate);
-    this.setState({ startDate: nextWeekStartDate, calendarItems: newCalendar });
+    const resetCalendar = resetCalendarItem(nextWeekStartDate);
+    setStartDate(nextWeekStartDate);
+    updateCalendarItems(resetCalendar);
   };
 
-  prevWeek = () => {
-    const { startDate } = this.state;
+  const prevWeek = () => {
     const prevWeekStartDate = new Date(startDate);
     prevWeekStartDate.setDate(prevWeekStartDate.getDate() - 4);
-    const newCalendar = this.updateCalendarItem(prevWeekStartDate);
-    this.setState({ startDate: prevWeekStartDate, calendarItems: newCalendar });
+    const resetCalendar = resetCalendarItem(prevWeekStartDate);
+    setStartDate(prevWeekStartDate);
+    updateCalendarItems(resetCalendar);
   };
 
-  calculateDayDate = (startDate, dayIndex) => {
+  const calculateDayDate = (startDate, dayIndex) => {
     const date = new Date(startDate);
     date.setDate(date.getDate() + dayIndex);
     return date.toLocaleDateString();
   };
 
-  renderWeekdays = () => {
+  const renderWeekdays = () => {
     const weekdays = [
       "Sunday",
       "Monday",
@@ -181,7 +147,7 @@ class Calendar extends React.Component {
       "Friday",
       "Saturday",
     ];
-    const startDayIndex = this.state.startDate.getDay();
+    const startDayIndex = startDate.getDay();
 
     const weekdayElements = Array.from({ length: 4 }, (_, i) => {
       const weekdayIndex = (startDayIndex + i) % 7;
@@ -190,9 +156,7 @@ class Calendar extends React.Component {
       return (
         <th key={day}>
           <span className="day">{day}</span>
-          <span className="date">
-            {this.calculateDayDate(this.state.startDate, i)}
-          </span>
+          <span className="date">{calculateDayDate(startDate, i)}</span>
         </th>
       );
     });
@@ -200,8 +164,8 @@ class Calendar extends React.Component {
     return <tr className="calendar-header">{weekdayElements}</tr>;
   };
 
-  handleAvailabilityChange = (i, j) => {
-    let previous_calendar = this.state.calendarItems;
+  const handleAvailabilityChange = (i, j) => {
+    let previous_calendar = [...calendarItems];
 
     const old_avail = previous_calendar[i].items[j].availability;
     if (old_avail === "") {
@@ -212,134 +176,208 @@ class Calendar extends React.Component {
       previous_calendar[i].items[j].availability = "";
     }
 
-    this.setState({ calendarItems: previous_calendar });
+    setCalendarItems(previous_calendar);
   };
 
-  updateAvailabilities = (e) => {
+  const updateAvailabilities = (e) => {
     // delete all availabilities for the days
     // add availabilities
-    if (this.state.token == "") {
-      this.fetchToken().then((token) => {
-        this.setAvailability(this.state.logged_in_email, token);
-      });
+    if (token !== "") {
+      setAvailability(loggedInEmail, token);
       return;
     }
-    this.setAvailability(this.state.logged_in_email, this.state.token);
+    // TODO: handle error with token
   };
 
-  render() {
-    return (
-      <div className="calendar">
-        <div className="calendar-header">
-          <h2>
-            {this.calculateDayDate(this.state.startDate, 0)} -{" "}
-            {this.calculateDayDate(this.state.startDate, 3)}
-          </h2>
-          <div className="calendar-color-index">
-            <div className="calendar-index-container">
-              <p>Available:</p>
-              <div className="calendar-available-box secondary-green-background"></div>
-            </div>
-            <div className="calendar-index-container">
-              <p>Preferred:</p>
-              <div className="calendar-preferred-box blue-background"></div>
-            </div>
-          </div>
-        </div>
-        <div className="calendar-container">
-          <div className="week-buttons">
-            <button
-              onClick={this.prevWeek}
-              className="button-primary prev-week"
-            >
-              Previous Week
-            </button>
-            <button
-              onClick={this.nextWeek}
-              className="button-primary prev-week"
-            >
-              Next Week
-            </button>
+  const [startDate, setStartDate] = useState(new Date());
+  const [token, setToken] = useState("");
+  const [loggedInEmail, setLoggedInEmail] = useState("user1@user1.com");
+  const [loggedInUserId, setLoggedInUserId] = useState(1);
+  const [calendarItems, setCalendarItems] = useState(
+    Array.from({ length: 48 }, (_, i) => {
+      return {
+        key: i / 2,
+        hour: Math.floor(i / 2),
+        time:
+          String(Math.floor(i / 2)).padStart(2, "0") +
+          (i % 2 == 0 ? ":00" : ":30"),
+        items: Array.from({ length: 4 }, (_, j) => {
+          return {
+            time_start:
+              String(
+                new Date(calculateDayDate(new Date(), j))
+                  .toISOString()
+                  .slice(0, 10)
+              ) +
+              "T" +
+              String(Math.floor(i / 2)).padStart(2, "0") +
+              (i % 2 == 0 ? ":00" : ":30"),
+            availability: "",
+          };
+        }),
+      };
+    })
+  );
 
-            {this.props.editable ? (
-              <button
-                onClick={this.updateAvailabilities}
-                className="button-primary"
-              >
-                Save Availability
-              </button>
-            ) : (
-              <></>
-            )}
+  const updateCalendarItems = async (resetCalendar) => {
+    const response = await axios.get(`${EVENT_URL}${props.event_id}/`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.statusText === "OK") {
+      let new_calendar = [...resetCalendar];
+      response.data.availabilities.forEach((availability) => {
+        if (availability.person == loggedInUserId) {
+          resetCalendar.forEach((item, i) => {
+            item.items.forEach((piece, j) => {
+              if (piece.time_start === availability.start_time.slice(0, 16)) {
+                new_calendar[i].items[j].availability = availability.type;
+              }
+            });
+          });
+        }
+      });
+      setCalendarItems(new_calendar);
+    }
+  };
+
+  useEffect(() => {
+    fetchToken();
+  }, []);
+
+  useEffect(() => {
+    const getCalendarItems = async () => {
+      const response = await axios.get(`${EVENT_URL}${props.event_id}/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.statusText === "OK") {
+        let new_calendar = [...calendarItems];
+        response.data.availabilities.forEach((availability) => {
+          if (availability.person == loggedInUserId) {
+            calendarItems.forEach((item, i) => {
+              item.items.forEach((piece, j) => {
+                if (piece.time_start === availability.start_time.slice(0, 16)) {
+                  new_calendar[i].items[j].availability = availability.type;
+                }
+              });
+            });
+          }
+        });
+        setCalendarItems(new_calendar);
+      }
+    };
+
+    if (token !== "") {
+      getCalendarItems();
+    }
+  }, [token]);
+
+  return (
+    <div className="calendar">
+      <div className="calendar-header">
+        <h2>
+          {calculateDayDate(startDate, 0)} - {calculateDayDate(startDate, 3)}
+        </h2>
+        <div className="calendar-color-index">
+          <div className="calendar-index-container">
+            <p>Available:</p>
+            <div className="calendar-available-box secondary-green-background"></div>
           </div>
-          <table>
-            <thead>{this.renderWeekdays()}</thead>
-            <tbody>
-              {this.state.calendarItems.map((row, index) => (
-                <>
-                  <tr key={row.key}>
-                    <td className="time">{row.time}</td>
-                    <td
-                      onClick={() => {
-                        this.handleAvailabilityChange(index, 0);
-                      }}
-                      data-block={row.items[0].time_start}
-                      style={
-                        row.items[0].availability === "preferred"
-                          ? { backgroundColor: "#1400ff" }
-                          : row.items[0].availability === "available"
-                          ? { backgroundColor: "#03a200" }
-                          : { backgroundColor: "transparent" }
-                      }
-                    ></td>
-                    <td
-                      onClick={() => {
-                        this.handleAvailabilityChange(index, 1);
-                      }}
-                      data-block={row.items[1].time_start}
-                      style={
-                        row.items[1].availability === "preferred"
-                          ? { backgroundColor: "#1400ff" }
-                          : row.items[1].availability === "available"
-                          ? { backgroundColor: "#03a200" }
-                          : { backgroundColor: "transparent" }
-                      }
-                    ></td>
-                    <td
-                      onClick={() => {
-                        this.handleAvailabilityChange(index, 2);
-                      }}
-                      data-block={row.items[2].time_start}
-                      style={
-                        row.items[2].availability === "preferred"
-                          ? { backgroundColor: "#1400ff" }
-                          : row.items[2].availability === "available"
-                          ? { backgroundColor: "#03a200" }
-                          : { backgroundColor: "transparent" }
-                      }
-                    ></td>
-                    <td
-                      onClick={() => {
-                        this.handleAvailabilityChange(index, 3);
-                      }}
-                      data-block={row.items[3].time_start}
-                      style={
-                        row.items[3].availability === "preferred"
-                          ? { backgroundColor: "#1400ff" }
-                          : row.items[3].availability === "available"
-                          ? { backgroundColor: "#03a200" }
-                          : { backgroundColor: "transparent" }
-                      }
-                    ></td>
-                  </tr>
-                </>
-              ))}
-            </tbody>
-          </table>
+          <div className="calendar-index-container">
+            <p>Preferred:</p>
+            <div className="calendar-preferred-box blue-background"></div>
+          </div>
         </div>
       </div>
-    );
-  }
-}
+      <div className="calendar-container">
+        <div className="week-buttons">
+          <button onClick={prevWeek} className="button-primary prev-week">
+            Previous Week
+          </button>
+          <button onClick={nextWeek} className="button-primary prev-week">
+            Next Week
+          </button>
+
+          {props.editable ? (
+            <button onClick={updateAvailabilities} className="button-primary">
+              Save Availability
+            </button>
+          ) : (
+            <></>
+          )}
+        </div>
+        <table>
+          <thead>{renderWeekdays()}</thead>
+          <tbody>
+            {calendarItems.map((row, index) => (
+              <>
+                <tr key={row.key}>
+                  <td className="time">{row.time}</td>
+                  <td
+                    onClick={() => {
+                      handleAvailabilityChange(index, 0);
+                    }}
+                    data-block={row.items[0].time_start}
+                    style={
+                      row.items[0].availability === "preferred"
+                        ? { backgroundColor: "#1400ff" }
+                        : row.items[0].availability === "available"
+                        ? { backgroundColor: "#03a200" }
+                        : { backgroundColor: "transparent" }
+                    }
+                  ></td>
+                  <td
+                    onClick={() => {
+                      handleAvailabilityChange(index, 1);
+                    }}
+                    data-block={row.items[1].time_start}
+                    style={
+                      row.items[1].availability === "preferred"
+                        ? { backgroundColor: "#1400ff" }
+                        : row.items[1].availability === "available"
+                        ? { backgroundColor: "#03a200" }
+                        : { backgroundColor: "transparent" }
+                    }
+                  ></td>
+                  <td
+                    onClick={() => {
+                      handleAvailabilityChange(index, 2);
+                    }}
+                    data-block={row.items[2].time_start}
+                    style={
+                      row.items[2].availability === "preferred"
+                        ? { backgroundColor: "#1400ff" }
+                        : row.items[2].availability === "available"
+                        ? { backgroundColor: "#03a200" }
+                        : { backgroundColor: "transparent" }
+                    }
+                  ></td>
+                  <td
+                    onClick={() => {
+                      handleAvailabilityChange(index, 3);
+                    }}
+                    data-block={row.items[3].time_start}
+                    style={
+                      row.items[3].availability === "preferred"
+                        ? { backgroundColor: "#1400ff" }
+                        : row.items[3].availability === "available"
+                        ? { backgroundColor: "#03a200" }
+                        : { backgroundColor: "transparent" }
+                    }
+                  ></td>
+                </tr>
+              </>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default Calendar;
